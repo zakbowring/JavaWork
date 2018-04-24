4b7c1ecde610b556ca6c478e165233545bfe0854
zakbowring <zbowring@sky.com>
2018-04-24 01:24:53 +0100

    First upload

---
+++ MusicSocialNetwork/src/music/scoial/network/MusicScoialNetwork.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package music.scoial.network;
+
+/**
+ *
+ * @author zakbo
+ */
+public class MusicScoialNetwork {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
